e5f89e5d9950fe9133b4cf7070322f683cda27b5
selim zaouali <zaouali.mohamed.selim@gmail.com>
2022-02-16 16:17:50 +0000

    fix

--- src/utils/authService.js
+++ src/utils/authService.js
@@ -1,10 +1,13 @@
 import createAuth0Client from "@auth0/auth0-spa-js";
 import { user, isAuthenticated, popupOpen } from "./store";
 
+export const DOMAIN =process.env.DOMAIN
+export const CLIENT_ID = process.env.CLIENTID
+
 async function createClient() {
   let auth0Client = await createAuth0Client({
-    domain: process.env.DOMAIN,
-    client_id: process.env.CLIENTID
+    domain: DOMAIN,
+    client_id: CLIENT_ID
   });
 
   return auth0Client;
